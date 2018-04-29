7a9c8927b0fd543b1196463f444a1b5c2f27d5e7
crea <654321su@gmail.com>
2018-04-29 17:32:01 +0900

    예외 발생, 발생하지 않는 코드 예제

---
+++ QuickPythonProgramming/ex_4-2-1.py
@@ -0,0 +1,8 @@
+# -*- conding : utf-8 -*-
+# 예외 발생 코드
+
+a = 0
+if a & 10 / a :
+    print("a가 0입니다.")
+else:
+    print("에러 없이 통과")
---
+++ QuickPythonProgramming/ex_4-2-2.py
@@ -0,0 +1,8 @@
+# # -*- conding : utf-8 -*-
+# 예외가 발생하지 않는 코드
+
+a = 0
+if a and 10 / a :
+    print("a가 0입니다.")
+else:
+    print("에러 없이 통과")
